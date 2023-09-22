400eb7a9786b4753b96e4a609633d1939fdb1f13
hykim <hiyoung.kimmy@gmail.com>
2023-09-22 13:07:08 -0700

    Fix : Bug Fixed

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 import { styles } from '../style';
 import { github } from '../assets';
 import { webLogo } from '../assets';
-
+//Wrapper
 import { SectionWrapper } from '../hoc';
 import { fadeInWork } from "../utils/motion";
 
