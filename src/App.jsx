b4fdd5765d93f4e84b08466a99a94572be4fa5c0
hykim <hiyoung.kimmy@gmail.com>
2023-10-20 13:03:27 -0700

    Add Mouse Cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { BrowserRouter } from 'react-router-dom';
 import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
-
+import { useRef } from 'react'; 
+import './App.css';
 
 function App() {
-
+  const cursor = useRef(null)
+  const changePosition = (e) => {
+    cursor.current.style.top = `${e.clientY}px`;
+    cursor.current.style.left = `${e.clientX}px`;
+  }
   return (
     <BrowserRouter>
-      <div className="relative z-0 bg-primary">
+      <div className="relative z-0 bg-primary" onMouseMove={changePosition}>
+      <div className="cursor-style" ref={cursor} ></div>
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
