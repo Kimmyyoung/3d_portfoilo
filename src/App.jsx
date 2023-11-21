cb82d6d825791387813823fd16cca1de705d4b89
hykim <hiyoung.kimmy@gmail.com>
2023-11-21 14:30:01 -0800

    Add : Google Analystics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,18 @@ import { BrowserRouter } from 'react-router-dom';
 import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 import { useRef } from 'react'; 
 import './App.css';
+import ReactGA from 'react-ga';
+import RouteChangeTracker from './RouteChangeTracker';
+
+ReactGA.event({
+  category: 'User',
+  action: 'Created an Account'
+});
+ReactGA.exception({
+  description: 'An error ocurred',
+  fatal: true
+});
+
 
 function App() {
   const cursor = useRef(null)
@@ -9,9 +21,16 @@ function App() {
     cursor.current.style.top = `${e.clientY}px`;
     cursor.current.style.left = `${e.clientX}px`;
   }
+
+  // eslint-disable-next-line no-undef
+  const TRACKING_ID = 'G-4TZCPENXZ7';
+
+  
+    ReactGA.initialize(TRACKING_ID);
   
   return (
     <BrowserRouter>
+      <RouteChangeTracker />
       <div className="relative z-0 bg-primary dark:bg-slate-50" onMouseMove={changePosition}>
       <div className="cursor-style" ref={cursor} ></div>
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
